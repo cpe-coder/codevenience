49b930d2121e69287be199f88d2174133d9091f6
Grian Gajila <griangajila2@gmail.com>
2025-07-20 16:23:29 +0800

    working on sign-in components

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,13 @@
+import CustomLogo from "@/components/custom-logo";
 import SignInForm from "@/components/form/SignInForm";
 
 export default function SignInPage() {
 	return (
-		<div>
-			<SignInForm />
+		<div className="flex flex-row items-center justify-between h-screen w-full p-8">
+			<CustomLogo />
+			<div>
+				<SignInForm />
+			</div>
 		</div>
 	);
 }
---
+++ components/custom-logo.tsx
@@ -0,0 +1,21 @@
+import { Scan } from "lucide-react";
+
+const CustomLogo = () => {
+	return (
+		<div className="flex gap-2 items-center">
+			<div className="p-2 rounded-2xl bg-gradient-to-br from-purple-500 to-indigo-500 text-white flex items-center gap-2">
+				<Scan size={60} />
+			</div>
+			<div className="gap-1 flex flex-col">
+				<h1 className="bg-gradient-to-r text-5xl pb-2 font-bold from-purple-500 to-indigo-500 bg-clip-text text-transparent">
+					Margie Codevenience
+				</h1>
+				<p className="text-2xl font-medium text text-slate-500">
+					Smart Business Management Platform
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default CustomLogo;
