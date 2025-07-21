fe8251846bfb6dbb96531606b4cd96544906f1a8
Grian Gajila <griangajila2@gmail.com>
2025-07-21 16:32:34 +0800

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import CustomLogo from "@/components/custom-logo";
+import * as motion from "motion/react-client";
+import { useRouter } from "next/navigation";
+
+import { useEffect } from "react";
 export default function Home() {
+	const router = useRouter();
+
+	useEffect(() => {
+		setTimeout(() => {
+			router.push("/sign-in");
+		}, 2000);
+	});
+
 	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			Hello World!
+		<div className="h-screen w-full flex items-center justify-center p-8">
+			<motion.div
+				initial={{ opacity: 0, scale: 0 }}
+				animate={{ opacity: 1, scale: 1 }}
+				transition={{
+					duration: 0.4,
+					scale: { type: "spring", visualDuration: 1.5, bounce: 0.5 },
+				}}
+			>
+				<CustomLogo />
+			</motion.div>
 		</div>
 	);
 }
