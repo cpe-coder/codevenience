10453d1ff598ae8991b53c3f2ab42f7d23982b17
Grian Gajila <griangajila2@gmail.com>
2025-07-24 01:25:16 +0800

    wip

--- app/(base)/profile/page.tsx
+++ app/(base)/profile/page.tsx
@@ -26,9 +26,31 @@ export default function Profile() {
 				</div>
 			</div>
 			<div className="flex space-x-6 items-center">
-				<div className="shadow rounded-md w-56">
-					<div className="p-2 border-2 w-14 h-14 rounded-full border-gray-400">
-						<UserRound color="gray" />
+				<div>
+					<div className="flex flex-col space-y-3 justify-center text-center shadow rounded-md w-90 p-6">
+						<div className="flex-1 items-center justify-center p-6 border-4 border-purple-500/20 self-center rounded-full">
+							<UserRound className="w-12 h-12" color="gray" />
+						</div>
+						<div className="text-center">
+							<h1 className="text-2xl font-medium text-slate-700">
+								Maria Santos
+							</h1>
+							<h2 className="text-md font-normal text-slate-500">
+								Store Manager
+							</h2>
+							<p className="text-xs font-normal text-slate-400">Operations</p>
+						</div>
+						<div>
+							<Button className="bg-purple-500/10 rounded-full text-purple-500">
+								Manager
+							</Button>
+						</div>
+						<p className="text-sm font-normal text-slate-500">
+							Oversee operations and manage team members
+						</p>
+					</div>
+					<div className="flex flex-col justify-center text-center shadow rounded-md w-90 p-6">
+						<h1>Simulate User Role:</h1>
 					</div>
 				</div>
 				<div className="shadow">hellow</div>
